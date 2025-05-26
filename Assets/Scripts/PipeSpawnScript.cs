e3271ae39eae24ea7d25b72456d2425e3cbad647
developerfatma <bulutfatmatugce@yahoo.com>
2025-05-26 16:16:35 +0300

    strings updated.

---
+++ Assets/Scripts/PipeSpawnScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+    public float maxTime = 1;
+    public float timer = 0;
+    public float height;
+    public GameObject pipe;
+    void Start()
+    {
+        GameObject newpipe = Instantiate(pipe);
+        newpipe.transform.position = transform.position + new Vector3(0,UnityEngine.Random.Range(-height, height), 0);
+    }
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = transform.position + new Vector3(0,UnityEngine.Random.Range(-height, height), 0);
+            Destroy(newpipe,10);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
