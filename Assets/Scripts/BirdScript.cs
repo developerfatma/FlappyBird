21b8535f0b4c6a9ceba1dff9206bce2c2ee30335
developerfatma <bulutfatmatugce@yahoo.com>
2025-05-28 23:30:38 +0300

    Score system aded

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdScript : MonoBehaviour
 {
     public float jump;
     Rigidbody2D rb;
+    public Text ScoreText;
+    public float score;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        score = 0;
     }
 
     void Update()
@@ -16,6 +20,15 @@ public class BirdScript : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * jump;
+        }
+        ScoreText.text = score.ToString();
+    }
+
+    void TriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag =="Scorer")
+        {
+            score++; 
         }
     }
 }
